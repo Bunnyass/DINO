a7121d5d29fa10e0637068a97384cb324c1548b7
Bunnyass <jayantnaik98@gmail.com>
2020-06-02 21:32:14 +0530

    Update

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
-import pyautogui
-from PIL import Image, ImageGrab
+import pyautogui # pip install pyautogui
+from PIL import Image, ImageGrab # pip install pillow
+# from numpy import asarray
 import time
 
 def hit(key):
     pyautogui.keyDown(key)
-
+    return
 
 def isCollide(data):
 
-    #Bird
-    for i in range(750,770):
-        for j in range(205,283):
-            if data[i,j] > 150:
-                hit("down")
-                return True
-    
-    #Cactus
-    for i in range(750,770):
-        for j in range(283,320):
-            if data[i,j] > 150:
+    for i in range(760, 780):
+        for j in range(283, 325):
+            if data[i, j] > 150:
                 hit("up")
                 return True
-    return False  
+    return False
 
-if __name__ == '__main__':
-    print("Hey, Dino game is about to start in 3 seconds: ")
+if __name__ == "__main__":
+    print("Hey.. Dino game about to start in 3 seconds")
     time.sleep(3)
-    hit("up")
+    pyautogui.PAUSE = 0.01 
+
     while True:
-        image = ImageGrab.grab()
+        image = ImageGrab.grab().convert('L') 
         data = image.load()
         isCollide(data)
+        
         """
-        #Draw the rectangle for cactus
-        for i in range(700,730):
-            for j in range(283,320):
-                data[i,j] = 0
-
-        #Draw the rectangle for bird
-        for i in range(700,730):
-            for j in range(205,283):
-                data[i,j] = 166
-
+        # Draw the rectangle for cactus
+        for i in range(760, 780):
+            for j in range(283, 325):
+                data[i, j] = 0
+        
         image.show()
         break
     """
     
-    
-    
+
